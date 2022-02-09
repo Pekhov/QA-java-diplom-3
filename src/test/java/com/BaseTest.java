cb958b1d4c19a53f8644cfbb1a275818fc609a7e
Pekhov <alexey@pekhov.com>
2022-02-09 17:24:32 +0300

    Убрал параметр при котором браузер держался открытым

--- src/test/java/com/BaseTest.java
+++ src/test/java/com/BaseTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class BaseTest {
 
     BaseTest() {
-        Configuration.holdBrowserOpen = true;
         Configuration.browser = "chrome";
         //Yandex browser
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pekhov\\Downloads\\yandexdriver\\yandexdriver.exe");
